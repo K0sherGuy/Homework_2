c29c746965fefbbbd931cc96d612daad9157e2db
Daniil <daniil.goihinberg@gmail.com>
2022-03-24 20:34:02 +0200

    My second homework

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DrivingLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your year of birth:");
+            int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            if (2022 - YearOfBirth < 18)
+            {
+                Console.WriteLine("You are too young to drive!");
+            } else if (2022 - YearOfBirth > 18)
+            {
+                Console.WriteLine("Drive carefully!");
+            } else
+            {
+                Console.WriteLine("Congratulations! You may apply for your driving lisence now.");
+            }
+        }
+    }
+}
